5123c0b4144c8d79e7247dfb4940c0d53588f18f
NFPereira <nilsonfpereira@hotmail.com>
2023-12-31 15:20:01 -0300

    Ajustes pages

--- src/pages/Operacoes/EntradaNotaXml/index.jsx
+++ src/pages/Operacoes/EntradaNotaXml/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const EntradaNotaXml = () => {
   return (
-    <div><h1>Entrada Nota (Xml)</h1></div>
-  )
-}
+    <div>
+      <h1>Entrada Nota (Xml)</h1>
+    </div>
+  );
+};
 
-export default EntradaNotaXml
+export default EntradaNotaXml;
--- src/pages/Operacoes/Venda/index.jsx
+++ src/pages/Operacoes/Venda/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Venda = () => {
   return (
-    <div><h1>Venda</h1></div>
-  )
-}
+    <div>
+      <h1>Venda</h1>
+    </div>
+  );
+};
 
-export default Venda
+export default Venda;
