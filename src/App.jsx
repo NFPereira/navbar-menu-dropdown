588c8aac909772a5d2c586e275b9ee523181d4b1
NFPereira <nilsonfpereira@hotmail.com>
2023-12-31 14:48:06 -0300

    Criação das Rotas e lista d menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React from "react";
 
 //import react-router-dom
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Paises from "./pages/Cadastros/Paises";
+import Estados from "./pages/Cadastros/Estados";
+import Cidades from "./pages/Cadastros/Cidades";
+import Navbar from "./components/Navbar";
+import EntradaNota from "./pages/Operacoes/EntradaNota";
+import EntradaNotaXml from "./pages/Operacoes/EntradaNotaXml";
+import EstornoNota from "./pages/Operacoes/EstornoEntrada";
+import Venda from "./pages/Operacoes/Venda";
+import EstornoVenda from "./pages/Operacoes/EstornoVenda";
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
+        {/* Rotas Cadastros */}
+        <Route path="/cadastros/paises" element={<Paises />} />
+        <Route path="/cadastros/estados" element={<Estados />} />
+        <Route path="/cadastros/cidades" element={<Cidades />} />
+        {/* Rotas Operações */}
+        <Route path="/operacoes/entrada-nota" element={<EntradaNota />} />
+        <Route
+          path="/operacoes/entrada-nota-xml"
+          element={<EntradaNotaXml />}
+        />
+        <Route path="/operacoes/estorno-nota" element={<EstornoNota />} />
+        <Route path="/operacoes/venda" element={<Venda />} />
+        <Route path="/operacoes/estorno-venda" element={<EstornoVenda />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className={styles.navbar}>
+        <div className={styles.brand}>
+          <Link>Navbar Menu Dropdown</Link>
+        </div>
+        <div className={styles.menu}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link>Cadastros</Link>
+              <ul>
+                <li>
+                  <Link to="/cadastros/paises">Países</Link>
+                </li>
+                <li>
+                  <Link to="/cadastros/estados">Estados</Link>
+                </li>
+                <li>
+                  <Link to="/cadastros/cidades">Cidades</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link>Operações</Link>
+              <ul>
+                <li>
+                  <Link to="/operacoes/entrada-nota">Entrada Nota</Link>
+                </li>
+                <li>
+                  <Link to="/operacoes/entrada-nota-xml">
+                    Entrada Nota (XML)
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/operacoes/estorno-entrada">Estorno Entrada</Link>
+                </li>
+                <li>
+                  <Link to="/operacoes/venda">Venda</Link>
+                </li>
+                <li>
+                  <Link to="/operacoes/estorno-venda">Estorno Venda</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link>Relatórios</Link>
+            </li>
+            <li>
+              <Link>Gráficos</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
